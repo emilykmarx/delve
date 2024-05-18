1bc076a8ccc0dfa860c8b16c08b5d403163557ec
Emily Marx <emilykmarx@alumni.stanford.edu>
2024-05-17 17:20:15 -0700

    Client uses rr reverse-step to get PC for hardware watchpoint hit.
    (Should work even if prev instr was a jump.)
    Run dlv server with --backend=rr (rr 5.7.0)

--- cmd/dlv/dlv_config_client/main.go
+++ cmd/dlv/dlv_config_client/main.go
@@ -16,23 +16,37 @@ var (
 	current_scope = api.EvalScope{GoroutineID: -1, Frame: 0}
 )
 
-/* Get instruction and source line corresponding to preceding PC in the function,
- * assuming preceding PC did not branch. */
-func PCToPrevPCLine(client *rpc2.RPCClient, PC uint64) (api.AsmInstruction, string) {
-	fct_instrs, err := client.DisassemblePC(current_scope, PC, api.IntelFlavour) // dst, src
+/* Get instruction and source line corresponding to preceding PC, for selected goroutine */
+func PCToPrevPCLine(client *rpc2.RPCClient, pc uint64) (api.AsmInstruction, string) {
+	/* TODO for now, assume goroutine that hit the bp is currently selected.
+	 * Should instead switch to that goroutine (need to read doc carefully:
+	 * https://github.com/go-delve/delve/blob/master/Documentation/api/ClientHowto.md#using-rpcservercommand)
+	 * (remember to pass that scope to Disass)
+	 * May be convenient to do the check for stack resize at same time */
+
+	state, err := client.ReverseStepInstruction(false)
 	if err != nil {
-		log.Fatalf("Error getting instructions at PC 0x%x: %v\n", PC, err)
+		log.Fatalf("Error reverse-stepping at PC 0x%x: %v\n", pc, err)
+	}
+	prev_pc := state.SelectedGoroutine.CurrentLoc.PC
+	state, err = client.StepInstruction(false)
+	if err != nil {
+		log.Fatalf("Error stepping at PC 0x%x: %v\n", prev_pc, err)
+	}
+	cur_pc := state.SelectedGoroutine.CurrentLoc.PC
+	if pc != cur_pc {
+		log.Fatalf("Failed to restore PC 0x%x; current PC is 0x%x\n", prev_pc, cur_pc)
 	}
 
-	var prev_instr api.AsmInstruction
-	for i, instr := range fct_instrs {
-		if instr.Loc.PC == PC {
-			if i == 0 {
-				log.Fatalf("No previous instruction for PC 0x%x\n", PC)
-			}
-			prev_instr = fct_instrs[i-1]
-		}
+	// end is exclusive
+	_prev_instr, err := client.DisassembleRange(current_scope, prev_pc, pc, api.IntelFlavour) // dst, src
+	if err != nil {
+		log.Fatalf("Error getting instructions at PC 0x%x: %v\n", prev_pc, err)
+	} else if len(_prev_instr) != 1 {
+		log.Fatalf("DisassembleRange returned %v instr for PC 0x%x\n", len(_prev_instr), prev_pc)
 	}
+
+	prev_instr := _prev_instr[0]
 
 	if os.Setenv("TERM", "dumb") != nil {
 		log.Fatalf("Error setting TERM=dumb")
@@ -73,7 +87,7 @@ func PCToPrevPCLine(client *rpc2.RPCClient, PC uint64) (api.AsmInstruction, stri
 }
 
 func main() {
-	fmt.Println("Starting delve config client\n")
+	fmt.Printf("Starting delve config client\n\n")
 	listenAddr := "localhost:4040"
 	client := rpc2.NewClient(listenAddr)
 
@@ -90,8 +104,9 @@ func main() {
 		log.Fatalf("Unexpected state %+v before hitting declaration of watch variable: %v\n", state, config_var)
 	}
 
-	// read-only not supported in delve yet
-	if _, err := client.CreateWatchpoint(current_scope, config_var, api.WatchRead|api.WatchWrite); err != nil {
+	// TODO this still triggers on writes as well -- seems to be an rr issue (`rwatch` does the same)
+	// Either fix rr, or ignore write hits (e.g. use rr to compare value before and after)
+	if _, err := client.CreateWatchpoint(current_scope, config_var, api.WatchRead); err != nil {
 		log.Fatalf("Error creating watchpoint at %v: %v\n", config_var, err)
 	}
 
@@ -106,19 +121,17 @@ func main() {
 		for _, thread := range state.Threads {
 			hit_bp := thread.Breakpoint
 			if hit_bp != nil {
-				instr, src_line := PCToPrevPCLine(client, thread.PC)
-				hit_loc := fmt.Sprintf("%v:%v:%v:0x%x \n%v \n%v",
-					thread.File, instr.Loc.Line, thread.Function.Name(),
-					instr.Loc.PC, instr.Text, src_line)
 				if hit_bp.WatchExpr != "" {
+					instr, src_line := PCToPrevPCLine(client, state.SelectedGoroutine.CurrentLoc.PC)
+					hit_loc := fmt.Sprintf("%v:%v:%v:0x%x \n%v \n%v",
+						instr.Loc.File, instr.Loc.Line, instr.Loc.Function.Name(),
+						instr.Loc.PC, instr.Text, src_line)
 					// TODO skip if due to stack resize (but not if same line does a resize and a real read)
 
-					/* Note PC has advanced one past the breakpoint by now.
-					 * TODO handle the case where this PC is in a different function than the breakpoint,
-					 * i.e. breakpoint was at a CALL. */
+					// Note PC has advanced one past the breakpoint by now, for hardware breakpoints (but not software)
 					fmt.Printf("Hit watchpoint for %v\nAt %v\n\n", hit_bp.WatchExpr, hit_loc)
 				} else {
-					fmt.Printf("Hit breakpoint at %v\n\n", hit_loc)
+					fmt.Printf("Hit breakpoint in %v\n\n", hit_bp.FunctionName)
 				}
 			}
 		}
