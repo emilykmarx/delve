4dbf024cb156495fb216a683c3fef0831e5a79f8
Emily Marx <emilykmarx@alumni.stanford.edu>
2024-05-20 15:25:15 -0700

    Reorg, partial progress on querying CodeQL for taint propagation
    Before realized that won't work since watch expr may not match
    triggering expr, due to e.g. aliasing

--- cmd/dlv/dlv_config_client/main.go
+++ cmd/dlv/dlv_config_client/main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"regexp"
-	"strings"
+	"os/exec"
 
-	"github.com/go-delve/delve/pkg/terminal"
 	"github.com/go-delve/delve/service/api"
 	"github.com/go-delve/delve/service/rpc2"
 )
@@ -16,74 +13,26 @@ var (
 	current_scope = api.EvalScope{GoroutineID: -1, Frame: 0}
 )
 
-/* Get instruction and source line corresponding to preceding PC, for selected goroutine */
-func PCToPrevPCLine(client *rpc2.RPCClient, pc uint64) (api.AsmInstruction, string) {
-	/* TODO for now, assume goroutine that hit the bp is currently selected.
-	 * Should instead switch to that goroutine (need to read doc carefully:
-	 * https://github.com/go-delve/delve/blob/master/Documentation/api/ClientHowto.md#using-rpcservercommand)
-	 * (remember to pass that scope to Disass)
-	 * May be convenient to do the check for stack resize at same time */
+const (
+	CODEQL_DB = "../codeql-home/test_df_db"
+)
 
-	state, err := client.ReverseStepInstruction(false)
-	if err != nil {
-		log.Fatalf("Error reverse-stepping at PC 0x%x: %v\n", pc, err)
-	}
-	prev_pc := state.SelectedGoroutine.CurrentLoc.PC
-	state, err = client.StepInstruction(false)
-	if err != nil {
-		log.Fatalf("Error stepping at PC 0x%x: %v\n", prev_pc, err)
-	}
-	cur_pc := state.SelectedGoroutine.CurrentLoc.PC
-	if pc != cur_pc {
-		log.Fatalf("Failed to restore PC 0x%x; current PC is 0x%x\n", prev_pc, cur_pc)
+/* Given a watched variable and a line on which a read watchpoint hits,
+ * query CodeQL for tainted variables */
+func PropagateTaint(watched_var string, file string, lineno int) {
+	WriteQueryFile(file, lineno)
+
+	cmd := exec.Command("codeql", "database", "analyze",
+		CODEQL_DB,
+		CODEQL_QUERY_FILE,
+		"--format=csv", "--output=tmp_codeql_results.csv",
+		"--threads=6", "--verbose", "--rerun")
+	if cmd.Run() != nil {
+		log.Fatalf("Error querying CodeQL for %v in %v:%v\n", watched_var, file, lineno)
 	}
 
-	// end is exclusive
-	_prev_instr, err := client.DisassembleRange(current_scope, prev_pc, pc, api.IntelFlavour) // dst, src
-	if err != nil {
-		log.Fatalf("Error getting instructions at PC 0x%x: %v\n", prev_pc, err)
-	} else if len(_prev_instr) != 1 {
-		log.Fatalf("DisassembleRange returned %v instr for PC 0x%x\n", len(_prev_instr), prev_pc)
-	}
+	ReadQueryOutput()
 
-	prev_instr := _prev_instr[0]
-
-	if os.Setenv("TERM", "dumb") != nil {
-		log.Fatalf("Error setting TERM=dumb")
-	}
-
-	t := terminal.New(client, nil)
-	lines, err := t.GetOutput(fmt.Sprintf("list %v:%v", prev_instr.Loc.File, prev_instr.Loc.Line))
-	if err != nil {
-		log.Fatalf("Error getting source code for PC 0x%x: %v\n", prev_instr.Loc.PC, err)
-	}
-
-	var src_line string
-	for _, line := range strings.Split(lines, "\n") {
-		var curline int
-		if strings.HasPrefix(line, "Showing") || line == "" {
-			continue
-		}
-		if _, err := fmt.Sscanf(line, " %d:", &curline); err != nil {
-			log.Fatalf("Failed to parse line number from line: %v: %v\n", line, err)
-		}
-		if curline != prev_instr.Loc.Line {
-			continue
-		}
-		re := regexp.MustCompile(`\s+(\d+):\s+`)
-		src_start := re.FindStringIndex(line)[1]
-		if src_start <= len(line)-1 {
-			src_line = line[src_start:]
-		} else {
-			log.Fatalf("Breakpoint PC 0x%x corresponds to empty line\n", prev_instr.Loc.PC)
-		}
-	}
-
-	if src_line == "" {
-		log.Fatalf("No source line found for PC 0x%x\n", prev_instr.Loc.PC)
-	}
-
-	return prev_instr, src_line
 }
 
 func main() {
@@ -104,7 +53,7 @@ func main() {
 		log.Fatalf("Unexpected state %+v before hitting declaration of watch variable: %v\n", state, config_var)
 	}
 
-	// TODO this still triggers on writes as well -- seems to be an rr issue (`rwatch` does the same)
+	// TODO this still triggers on writes as well -- seems to be an rr issue (`rwatch` does the same, even in cpp)
 	// Either fix rr, or ignore write hits (e.g. use rr to compare value before and after)
 	if _, err := client.CreateWatchpoint(current_scope, config_var, api.WatchRead); err != nil {
 		log.Fatalf("Error creating watchpoint at %v: %v\n", config_var, err)
@@ -130,6 +79,8 @@ func main() {
 
 					// Note PC has advanced one past the breakpoint by now, for hardware breakpoints (but not software)
 					fmt.Printf("Hit watchpoint for %v\nAt %v\n\n", hit_bp.WatchExpr, hit_loc)
+
+					PropagateTaint(hit_bp.WatchExpr, instr.Loc.File, instr.Loc.Line)
 				} else {
 					fmt.Printf("Hit breakpoint in %v\n\n", hit_bp.FunctionName)
 				}
