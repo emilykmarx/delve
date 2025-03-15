627055a528a52450166fb1e306cc3db24eade5c7
Emily Marx <emilykmarx@alumni.stanford.edu>
2025-03-15 13:29:52 -0700

    pre-commit hook: Add option to only run one group of tests

--- conftamer/pre-commit-hook.py
+++ conftamer/pre-commit-hook.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import subprocess
+import sys
+from collections import namedtuple
+
 
 subprocess.check_output('go install github.com/go-delve/delve/cmd/dlv', shell=True, text=True)
 subprocess.check_output('go build github.com/go-delve/delve/cmd/dlv/conftamer_main', shell=True, text=True)
@@ -14,20 +17,42 @@ No idea why, but running in loop fixes. Some state leftover between tests? Prett
 '''
 After editing this: remember to copy it to .git/hooks
 '''
-for i, test_group in enumerate(['Watchpoint', 'Client']):
-  if i == 0:
-    test_path = "pkg/proc"
-    grep_arg = f"'func TestWatchpoints' {test_path}/proc_test.go"
-  else:
-    test_path = "cmd/dlv"
-    grep_arg = f"'func Test' {test_path}/conftamer_test.go"
 
-  grep = subprocess.run(f"grep {grep_arg} | cut -d '(' -f1 | cut -d ' ' -f2", shell=True, check=True, text=True, capture_output=True)
+'''
+Optional argument to run only watchpoint or client tests
+'''
+
+nargs = len(sys.argv)
+TestGroup = namedtuple('TestGroup', ['test_path', 'grep_arg'])
+test_path = "pkg/proc"
+watchpoint_tests = TestGroup(test_path,  f"'func TestWatchpoints' {test_path}/proc_test.go")
+test_path = "cmd/dlv"
+client_tests = TestGroup(test_path, f"'func Test' {test_path}/conftamer_test.go")
+
+test_groups = {
+'watchpoint': watchpoint_tests,
+'client': client_tests
+}
+
+if nargs > 1:
+  arg = sys.argv[1]
+  if arg == 'watchpoint':
+    print('Only running watchpoint tests')
+    test_groups.pop('client')
+  elif arg == 'client':
+    print('Only running client tests')
+    test_groups.pop('watchpoint')
+  else:
+    print(f'Unknown arg {arg}')
+    exit(1)
+
+for test, test_group in test_groups.items():
+  grep = subprocess.run(f"grep {test_group.grep_arg} | cut -d '(' -f1 | cut -d ' ' -f2", shell=True, check=True, text=True, capture_output=True)
   tests = grep.stdout.splitlines()
-  print(f'{test_group} tests: {tests}')
+  print(f'{test} tests: {tests}')
 
   for test in tests:
-    test_cmd = f'/home/emily/projects/wtf_project/go1.20.1/bin/go test -v -timeout 30s -run {test} github.com/go-delve/delve/{test_path} -count=1 -failfast'
+    test_cmd = f'/home/emily/projects/wtf_project/go1.20.1/bin/go test -v -timeout 30s -run {test} github.com/go-delve/delve/{test_group.test_path} -count=1 -failfast'
     print(test_cmd)
     try:
       p = subprocess.run(test_cmd, shell=True, check=True, text=True, capture_output=True)
