3aed9b9a8aea5dce4fc2711a95461d35aa22b103
Emily Marx <emilykmarx@alumni.stanford.edu>
2025-05-07 16:38:56 -0700

    Revert logging in alocator_http back to where it was when saw problem =>
    can repro (at least second MoveObject finds a pointer to obj)

--- _fixtures/conftamer/allocator_http.go
+++ _fixtures/conftamer/allocator_http.go
@@ -22,10 +22,10 @@ func main() {
 		log.Println(http.ListenAndServe("localhost:6060", nil))
 	}()
 	// let server start
-	time.Sleep(1 * time.Second)                                           // w/ regular bp on server at l27: conn refused if 100ms, hangs if 1s
-	ptr := f()                                                            // *ptr is on heap
-	fmt.Printf("addr of obj %#x and of ptr %#x (in target)\n", ptr, &ptr) // set wp on *ptr => will move *ptr and update ptr
-	time.Sleep(1 * time.Second)                                           // do the move (don't want to access in meantime)
+	time.Sleep(1 * time.Second)        // w/ regular bp on server at l27: conn refused if 100ms, hangs if 1s
+	ptr := f()                         // *ptr is on heap
+	fmt.Printf("old addr: %#x\n", ptr) // set wp on *ptr => will move *ptr and update ptr
+	time.Sleep(1 * time.Second)        // do the move (don't want to access in meantime)
 	fmt.Println("target about to access")
 	x := *ptr // wp hit (for moved *ptr) => propagate to x
 	fmt.Println("target exit; %v", x)
